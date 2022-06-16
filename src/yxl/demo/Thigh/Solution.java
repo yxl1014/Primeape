6db33e51116f9b2a1707d69abed3f67bc88be4d5
yxl1014 <1627419257@qq.com>
2022-06-16 17:29:40 +0800

    mei tuan day 3

---
+++ src/yxl/demo/Thigh/Solution.java
@@ -0,0 +1,52 @@
+package yxl.demo.Thigh;
+
+import javafx.util.Pair;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeMap;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<Integer> ws = new ArrayList<>();
+        List<Integer> vs = new ArrayList<>();
+        String[] os = reader.readLine().split(" ");
+        int all = Integer.parseInt(os[0]), get = Integer.parseInt(os[1]);
+        if (all < get)
+            return;
+
+        for (int i = 0; i < all; i++) {
+            String[] wv = reader.readLine().split(" ");
+            ws.add(Integer.valueOf(wv[0]));
+            vs.add(Integer.valueOf(wv[1]));
+        }
+        ArrayList<Pair<Integer, Integer>> res = new ArrayList<>();
+        int[] wss = new int[all];
+        for (int i = 0; i < all; i++) {
+            int wwss = ws.get(i) + vs.get(i) * 2;
+            res.add(new Pair<>(wwss, i + 1));
+            wss[i] = wwss;
+        }
+
+        Arrays.parallelSort(wss);
+
+        for (int i = 0; i < get; i++) {
+            int w = wss[all - 1 - i];
+            for (Pair<Integer, Integer> p : res) {
+                if (p.getKey() == w) {
+                    writer.write(p.getValue() + " ");
+                    res.remove(p);
+                    break;
+                }
+            }
+        }
+
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+}
